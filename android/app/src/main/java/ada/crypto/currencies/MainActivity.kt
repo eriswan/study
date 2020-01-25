e5ee4b357bfaf7dc6416cd5c177c1d40449ec42a
Erika <erikaalves018@gmail.com>
2020-01-25 19:46:07 -0300

    first connection

--- android/app/src/main/java/ada/crypto/currencies/MainActivity.kt
+++ android/app/src/main/java/ada/crypto/currencies/MainActivity.kt
@@ -2,12 +2,22 @@ package ada.crypto.currencies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val txt by lazy { findViewById<TextView>(R.id.txt) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Requester().fetchHome({ currencies ->
+            txt.text = currencies.toString()
+        }, { error ->
+            txt.text = error.message
+            error.printStackTrace()
+        })
     }
 
 }
--- android/app/src/main/java/ada/crypto/currencies/Requester.kt
+++ android/app/src/main/java/ada/crypto/currencies/Requester.kt
@@ -4,11 +4,15 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.Exception
 
 class Requester {
 
-    private val retrofit = Retrofit.Builder().baseUrl("192.168.15.15:5000").build()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://192.168.15.15:5000")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     fun fetchHome(
 
